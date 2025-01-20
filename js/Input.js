e5598f0e5f5132d3419b65cc86abc811b7847754
matuiti <ichi.animo@gmail.com>
2025-01-20 17:10:07 +0900

    ...

--- js/Input.js
+++ js/Input.js
@@ -15,11 +15,11 @@ class Input {
     this.rightBtn = document.getElementById("right-btn");
 
     this.leftBtn.addEventListener("touchstart", this.touchLeftStart.bind(this), { passive: false });
-    this.leftBtn.addEventListener("touchend", this.stopMovement.bind(this), { passive: false });
+    this.leftBtn.addEventListener("touchend", this.touchLeftEnd.bind(this), { passive: false });
     this.leftBtn.addEventListener("touchcancel", this.stopMovement.bind(this), { passive: false });
 
     this.rightBtn.addEventListener("touchstart", this.touchRightStart.bind(this), { passive: false });
-    this.rightBtn.addEventListener("touchend", this.stopMovement.bind(this), { passive: false });
+    this.rightBtn.addEventListener("touchend", this.touchRightEnd.bind(this), { passive: false });
     this.rightBtn.addEventListener("touchcancel", this.stopMovement.bind(this), { passive: false });
 
     // PCのキーボードイベント
@@ -30,30 +30,53 @@ class Input {
     e.preventDefault();
     if (GAME_STATE.over || CONFIG.hit || SARU.x <= 0) return;
     this.moveLeft = true;
+    this.isSingleTouch = true;
     this.move();
+  }
+
+  touchLeftEnd(e) {
+    e.preventDefault();
+    if (this.isSingleTouch) {
+      SARU.move(false);
+      gameLoop();
+    }
+    this.stopMovement();
   }
 
   touchRightStart(e) {
     e.preventDefault();
     if (GAME_STATE.over || CONFIG.hit || SARU.x >= (CONFIG.SCREEN_W - SARU.w)) return;
     this.moveRight = true;
+    this.isSingleTouch = true;
     this.move();
+  }
+
+  touchRightEnd(e) {
+    e.preventDefault();
+    if (this.isSingleTouch) {
+      SARU.move(true);
+      gameLoop();
+    }
+    this.stopMovement();
   }
 
   stopMovement() {
     this.moveLeft = false;
     this.moveRight = false;
+    this.isSingleTouch = false;
   }
 
   move() {
     if (this.moveLeft && !GAME_STATE.over && !CONFIG.hit && SARU.x > CONFIG.LEFT_END) {
+      this.isSingleTouch = false;
       SARU.move(false);
       gameLoop();
-      setTimeout(this.move.bind(this), 200); // 速度調整のために200ms待機
+      setTimeout(this.move.bind(this), 300); // 速度調整のために300ms待機
     } else if (this.moveRight && !GAME_STATE.over && !CONFIG.hit && SARU.x < CONFIG.RIGHT_END) {
+      this.isSingleTouch = false;
       SARU.move(true);
       gameLoop();
-      setTimeout(this.move.bind(this), 200); // 速度調整のために200ms待機
+      setTimeout(this.move.bind(this), 300); // 速度調整のために300ms待機
     }
   }
 
