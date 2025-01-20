2f2d9bf6fb06524eca4149b009da7d9da180fec1
matuiti <ichi.animo@gmail.com>
2025-01-20 16:21:17 +0900

    ...

--- js/Input.js
+++ js/Input.js
@@ -14,11 +14,11 @@ class Input {
     this.leftBtn = document.getElementById("left-btn");
     this.rightBtn = document.getElementById("right-btn");
 
-    this.leftBtn.addEventListener("touchstart", this.touchLeft.bind(this), { passive: false });
+    this.leftBtn.addEventListener("touchstart", this.touchLeftStart.bind(this), { passive: false });
     this.leftBtn.addEventListener("touchend", this.stopMovement.bind(this), { passive: false });
     this.leftBtn.addEventListener("touchcancel", this.stopMovement.bind(this), { passive: false });
 
-    this.rightBtn.addEventListener("touchstart", this.touchRight.bind(this), { passive: false });
+    this.rightBtn.addEventListener("touchstart", this.touchRightStart.bind(this), { passive: false });
     this.rightBtn.addEventListener("touchend", this.stopMovement.bind(this), { passive: false });
     this.rightBtn.addEventListener("touchcancel", this.stopMovement.bind(this), { passive: false });
 
@@ -26,34 +26,40 @@ class Input {
     document.addEventListener('keydown', this.handleKeyDown.bind(this));
   }
 
-  touchLeft(e) {
+  touchLeftStart(e) {
     e.preventDefault();
     if (GAME_STATE.over || CONFIG.hit || SARU.x <= 0) return;
-    this.moveLeft = true;
-    this.move();
+    this.touchLeftTimeout = setTimeout(() => {
+      this.moveLeft = true;
+      this.move();
+    }, 200); // 200msの遅延を追加
   }
 
-  touchRight(e) {
+  touchRightStart(e) {
     e.preventDefault();
     if (GAME_STATE.over || CONFIG.hit || SARU.x >= (CONFIG.SCREEN_W - SARU.w)) return;
-    this.moveRight = true;
-    this.move();
+    this.touchRightTimeout = setTimeout(() => {
+      this.moveRight = true;
+      this.move();
+    }, 200); // 200msの遅延を追加
   }
 
   stopMovement() {
+    clearTimeout(this.touchLeftTimeout);
+    clearTimeout(this.touchRightTimeout);
     this.moveLeft = false;
     this.moveRight = false;
   }
 
   move() {
-    if (this.moveLeft && !GAME_STATE.over && !CONFIG.hit && SARU.x > 0) {
+    if (this.moveLeft && !GAME_STATE.over && !CONFIG.hit && SARU.x > CONFIG.LEFT_END) {
       SARU.move(false);
       gameLoop();
-      setTimeout(this.move.bind(this), 200); // 速度調整のために100ms待機
-    } else if (this.moveRight && !GAME_STATE.over && !CONFIG.hit && SARU.x < (CONFIG.SCREEN_W - SARU.w)) {
+      setTimeout(this.move.bind(this), 200); // 速度調整のために200ms待機
+    } else if (this.moveRight && !GAME_STATE.over && !CONFIG.hit && SARU.x < CONFIG.RIGHT_END) {
       SARU.move(true);
       gameLoop();
-      setTimeout(this.move.bind(this), 200); // 速度調整のために100ms待機
+      setTimeout(this.move.bind(this), 200); // 速度調整のために200ms待機
     }
   }
 
